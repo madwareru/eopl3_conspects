38a65d71b497420c53f70186215e1ffa1ed1e39d
madwareru <madware.ru@gmail.com>
2023-11-12 10:23:08 +0300

    Lambda expressions fun

--- exercises/src/main/kotlin/Main.kt
+++ exercises/src/main/kotlin/Main.kt
@@ -1,11 +1,13 @@
 import exercises.ex2_1
+import exercises.ex2_15
 import exercises.ex2_5
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
 val exerciseLookup = hashMapOf(
     "2.1" to ::ex2_1,
-    "2.5" to ::ex2_5
+    "2.5" to ::ex2_5,
+    "2.15" to ::ex2_15
 )
 
 fun main() {
---
+++ exercises/src/main/kotlin/exercises/Ex2_15.kt
@@ -0,0 +1,86 @@
+package exercises
+
+data class LambdaExpr<TLcExp, TVar>(val boundVar: TVar, val body: TLcExp)
+data class ApplicationExpr<TLcExp>(val rator: TLcExp, val rand: TLcExp)
+data class LcExpScope<TLcExp, TVar> (
+    // constructors:
+    val varExpr: (TVar) -> TLcExp,
+    val lambdaExpr: (TVar, TLcExp) -> TLcExp,
+    val appExpr: (TLcExp, TLcExp) -> TLcExp,
+
+    // extractors (predicates aren't needed):
+    val asVarExpr: (TLcExp) -> TVar?,
+    val asLambdaExpr: (TLcExp) -> LambdaExpr<TLcExp, TVar>?,
+    val asAppExpr: (TLcExp) -> ApplicationExpr<TLcExp>?
+)
+
+fun <TLcExp, TVar> LcExpScope<TLcExp, TVar>.occursFree(searchVar: TVar, exp: TLcExp): Boolean =
+    asVarExpr(exp)?.let { it == searchVar }
+        ?: asLambdaExpr(exp)?.let { it.boundVar != searchVar && occursFree(searchVar, it.body) }
+        ?: asAppExpr(exp)?.let { occursFree(searchVar, it.rator) || occursFree(searchVar, it.rand) }
+        ?: throw IllegalArgumentException("exp is neither var, lambda or app")
+
+sealed class LcExpression {
+    data class Var(val ident: String): LcExpression()
+    data class Lambda(val boundVarIdent: String, val body: LcExpression): LcExpression()
+    data class Application(val rator: LcExpression, val rand: LcExpression): LcExpression()
+}
+
+val lcExpressionScope = LcExpScope<LcExpression, String> (
+    varExpr = { LcExpression.Var(it) },
+    lambdaExpr = { boundIdent, body -> LcExpression.Lambda(boundIdent, body) },
+    appExpr = { rator, rand -> LcExpression.Application(rator, rand) },
+    asVarExpr = { (it as? LcExpression.Var)?.ident },
+    asLambdaExpr = { (it as? LcExpression.Lambda)?.let { l -> LambdaExpr(l.boundVarIdent, l.body) } },
+    asAppExpr = { (it as? LcExpression.Application)?.let { a -> ApplicationExpr(a.rator, a.rand) } }
+)
+
+fun ex2_15() {
+    fun<T> testScope(scope: () -> LcExpScope<T, String>): () -> Unit = {
+        with(scope()) {
+            val first = appExpr(
+                lambdaExpr(
+                    "a",
+                    appExpr(
+                        varExpr("a"),
+                        varExpr("b")
+                    )
+                ),
+                varExpr("c")
+            )
+            println("a occurs free in first? ${occursFree("a", first)}")
+            println("b occurs free in first? ${occursFree("b", first)}")
+            println("c occurs free in first? ${occursFree("c", first)}")
+            println("d occurs free in first? ${occursFree("d", first)}")
+
+            val second = lambdaExpr(
+                "x",
+                lambdaExpr(
+                    "y",
+                    appExpr(
+                        lambdaExpr(
+                            "x",
+                            appExpr(
+                                varExpr("x"),
+                                varExpr("y")
+                            )
+                        ),
+                        varExpr("x")
+                    )
+                )
+            )
+            println("x occurs free in second? ${occursFree("x", second)}")
+            println("y occurs free in second? ${occursFree("y", second)}")
+            println("z occurs free in second? ${occursFree("z", second)}")
+        }
+    }
+
+    arrayOf(
+        "data structure repr" to testScope { lcExpressionScope },
+    ).forEach {
+        val (label, testScope) = it
+        println("testing $label:")
+        testScope()
+        println()
+    }
+}
